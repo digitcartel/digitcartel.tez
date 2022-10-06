a1c1cff2857547cbd48bbdad9c3fbf0b34758e00
digitcartel <114265612+digitcartel@users.noreply.github.com>
2022-10-06 18:45:15 +0200

    Update _Activity.js

--- src/components/Activity/_Activity.js
+++ src/components/Activity/_Activity.js
@@ -27,7 +27,7 @@ export const Activity = ({ context }) => {
                       {e.domainName}
                     </button>
                     <h1 className="text-indigo-500 font-bold text-[2vw] lXs:text-[1vw] px-[1.5vw] lXs:px-[1vw]">
-                      {e.price / 10 ** 6}tz
+                      {(e.price / 10 ** 6).toFixed(1)}tz
                     </h1>
                   </div>
                 );
@@ -35,21 +35,21 @@ export const Activity = ({ context }) => {
           </div>
         </div>
       </div>
-      <div className="w-full lex flex-col mx-2">
+      <div className="w-4/12 lex flex-col mx-2">
         <div className="mb-2 w-full flex flex-row px-[2vw] lXs:px-[1vw] py-2 items-center bg-indigo-500 border-2 rounded-xl border-white">
           <h1 className="text-white text-[2.5vw] lXs:text-[1.5vw]">🎈OFFERS</h1>
         </div>
-        <div className="w-auto bg-white bg-opacity-10 border-indigo-500 border-2 mb-2 rounded-xl">
+        <div className="w-full bg-white bg-opacity-10 border-indigo-500 border-2 mb-2 rounded-xl">
           <div className="overflow-y-auto no-scroll-bar h-[10vh] lXs:h-[15vh] flex flex-col my-[1vw]">
             {context.state._LastOffers &&
               context.state._LastOffers.map((e, i) => {
                 return (
                   <div
                     key={i + "_lastOffers"}
-                    className="flex flex-row rounded-full px-[1vw] items-center justify-between"
+                    className="flex flex-row rounded-full px-[1vw] items-center"
                   >
                     {!e.token && (
-                      <h1 className="text-white text-[2vw] lXs:text-[1vw] px-[1.5vw] lXs:px-[1vw] whitespace-nowrap truncate w-full text-left">
+                      <h1 className="text-white text-[2vw] lXs:text-[1vw] px-[1.5vw] lXs:px-[1vw] whitespace-nowrap w-full truncate text-left">
                         {!e.token && (
                           <span className="text-purple-500 uppercase">
                             Collection
@@ -66,13 +66,13 @@ export const Activity = ({ context }) => {
                             _Viewed: e.token.name,
                           });
                         }}
-                        className="text-white text-[2vw] lXs:text-[1vw] px-[1.5vw] lXs:px-[1vw] whitespace-nowrap truncate w-full hover:bg-indigo-500 hover:text-white rounded-full text-left"
+                        className="text-white text-[2vw] lXs:text-[1vw] px-[1.5vw] lXs:px-[1vw] whitespace-nowrap w-full truncate hover:bg-indigo-500 hover:text-white rounded-full text-left"
                       >
                         {e.token.name}
                       </button>
                     )}
                     <h1 className="ml-auto text-indigo-500 font-bold text-[2vw] lXs:text-[1vw] px-[1.5vw] lXs:px-[1vw]">
-                      {e.price / 10 ** 6}tz
+                      {(e.price / 10 ** 6).toFixed(1)}tz
                     </h1>
                     {e.status === "concluded" && (
                       <h1 className="w-1 h-1 rounded-full bg-purple-500" />
