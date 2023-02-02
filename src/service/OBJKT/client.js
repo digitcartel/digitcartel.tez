366faba9673496322e62dd761388d0b993ee9c00
digitcartel <114265612+digitcartel@users.noreply.github.com>
2023-02-02 16:24:49 +0100

    Update client.js

--- src/service/OBJKT/client.js
+++ src/service/OBJKT/client.js
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const OBJKT = new ApolloClient({
-  uri: "https://data.objkt.com/v2/graphql",
+  uri: "https://data.objkt.com/v3/graphql",
   cache: new InMemoryCache(),
 });
